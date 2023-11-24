5c62a3f7919c1ae8c7b3410dc047bee631fe7537
Sofía Pinto <118233041+sofiapintogalvez@users.noreply.github.com>
2023-11-24 11:23:53 -0500

    Add files via upload

---
+++ FormCV/CV_page.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace FormCV
+{
+    public partial class CV : ContentPage
+    {
+        public CV(string nombre, string apellido, string ocupacion, string correo, string telefono)
+        {
+            InitializeComponent();
+
+            nombreLabel.Text = nombre;
+            apellidoLabel.Text = apellido;
+            ocupacionLabel.Text = ocupacion;
+            correoLabel.Text = correo;
+            telefonoLabel.Text = telefono;
+        }
+    }
+}
